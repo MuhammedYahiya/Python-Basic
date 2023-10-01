7af1363ee1c17896c7c5ec010853bfb52b3aa0f2
MuhammedYahiya <yahiyahmhd@gmail.com>
2023-10-01 16:51:44 +0530

    best complexity code for find second smallest and largest

--- Week 3/second_small_large.py
+++ Week 3/second_small_large.py
@@ -5,30 +5,22 @@ for i in range(n):
     number = int(input())
     arr.append(number)
 
-large = arr[0]
-second_large = arr[0]
-small = arr[0]
-second_small = float('inf')
+large = second_large = float('-inf')
+small = second_small = float('inf')
 
-for index in range(n):
-    if arr[index] > large:
-        large = arr[index]
+for num in arr:
+    if num > large:
+        second_large = large
+        large = num
+    elif num > second_large and num != large:
+        second_large = num
     
-    if arr[index] < small:
-        small = arr[index]
+    if num < small:
+        second_small = small
+        small = num
+    elif num < second_small and num != small:
+        second_small = num
 
-for index in range(n):
-    if arr[index] > second_large and arr[index] != large:
-        second_large = arr[index]
-    if arr[index] < second_small and arr[index] != small:
-        second_small = arr[index]
-    
-    
-
-    
-
-print("Second largest element is: ",second_large)
-print("Smallest element is: ",small)
-print("Seond smallest element is: ",second_small)
-
-
+print("Second largest element is: ", second_large)
+print("Smallest element is: ", small)
+print("Second smallest element is: ", second_small)
